d8aff133a9208ad949fa8f7c05d360118117d978
hackbras <hackbras@outlook.com>
2023-03-24 10:49:16 -0300

    First commit

---
+++ Tryitter/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tryitter.Models;
+
+namespace Tryitter.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+    public class PostController : Controller
+    {
+        [HttpPost]
+        public void Twitar()
+        {
+
+        }
+        [HttpGet]
+        public IEnumerable<Post> ConsultarPosts(){
+            return new List<Post>();
+        }
+        [HttpDelete]
+        public void Delete() { 
+            
+        }
+
+        [HttpPut]
+        public void Atualizacao()
+        {
+
+        }
+    }
+}
---
+++ Tryitter/Models/Post.cs
@@ -0,0 +1,8 @@
+﻿namespace Tryitter.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Twitte { get; set; }
+    }
+}
